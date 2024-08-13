007153492ba6a30d95f111fe2fc3fcbb6cc01581
cbility <ap.johnston@outlook.com>
2024-08-13 12:13:20 +0100

    correct typo

--- src/Ply/JSScripts/createQFQuote.ts
+++ src/Ply/JSScripts/createQFQuote.ts
@@ -25,7 +25,7 @@ const input = { // input for testing
 
 /////////////////////////////////PLY CODE///////////////////////////////
 
-const MS_IN_A_DAY = 24 * 60 * 0 * 1000;
+const MS_IN_A_DAY = 24 * 60 * 60 * 1000;
 let quoteItems = []; // set price items
 let quoteTasks = [] // hourly charged items
 
