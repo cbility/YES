4dd0223f7d37dc8efcd5cddde2001e0efaa1e258
cbility <ap.johnston@outlook.com>
2024-09-03 13:53:33 +0100

    restructure CI pipeline

--- src/lambda/ofgemCheck/indexGetOfgemCheckInputs.ts
+++ src/lambda/ofgemCheck/indexGetOfgemCheckInputs.ts
@@ -1,6 +1,6 @@
 //top level index file for lambda
 
-import handler from "../ofgemCheck/logOfgemCheckOutputs.js";
+import handler from "../../ofgemCheck/getOfgemCheckInputs.js";
 
 module.exports = {
     handler
