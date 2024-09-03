a94704a02639fd77d36d403a1b419fc04056f70b
cbility <ap.johnston@outlook.com>
2024-09-03 10:16:42 +0100

    change handler location test

---
+++ src/lambda/indexCheckScraperOutput.ts
@@ -0,0 +1,27 @@
+//lambda for parsing scraper outputs in step function and updating inputs for next batch
+
+export default async function handler(event: ScraperOutputEvent) {
+
+    console.log("INPUT EVENT: \n" + JSON.stringify(event, null, 2));
+
+    const batchIndex: number = event.inputBatches.current.batchIndex;
+    const updateSuccess = event.currentOutput.Payload.body.includes("Update Complete");
+    const isFinal: Boolean = event.inputBatches.current.isFinal;
+
+    event.inputBatches.all[batchIndex].done = true;
+    event.inputBatches.all[batchIndex].success = updateSuccess;
+
+    if (!isFinal) {
+        //set up next batch
+        event.inputBatches.current.inputs = JSON.stringify(event.inputBatches.all?.[batchIndex + 1]?.inputs);
+        event.inputBatches.current.batchIndex++;
+        event.inputBatches.current.isFinal = batchIndex >= event.inputBatches.all.length - 1;
+    }
+
+    console.log("OUTPUT EVENT: \n" + JSON.stringify(event, null, 2));
+    return event;
+}
+
+module.exports = {
+    handler
+};
