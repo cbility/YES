5d25ca663acccc0f750d788387140689976390bd
cbility <ap.johnston@outlook.com>
2024-09-06 01:49:14 +0100

    remove remaining ES code

--- packages/SmartSuite/src/getTables.ts
+++ packages/SmartSuite/src/getTables.ts
@@ -4,9 +4,7 @@ import SmartSuite from "./SmartSuiteAPIHandler.js";
 import { promises as fs } from 'fs';
 import path from 'path';
 import { dirname } from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const accountID: string = process.argv[2];
