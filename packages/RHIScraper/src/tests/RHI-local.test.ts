e5b99f3ea78aca1de20361d3eee2a7853d942df0
cbility <ap.johnston@outlook.com>
2024-09-08 21:48:17 +0100

    update test

--- packages/RHIScraper/src/tests/RHI-local.test.ts
+++ packages/RHIScraper/src/tests/RHI-local.test.ts
@@ -2,6 +2,8 @@ require("dotenv").config();
 import puppeteer from "puppeteer";
 import main from "../main/main.js";
 import { PuppeteerNode as PuppeteerCoreNode } from "puppeteer-core";
+import SmartSuite from "../../../SmartSuite/dist/SmartSuiteAPIHandler.js";
+import { loginsTable } from "../../../SmartSuite/dist/tables.js";
 
 const browserArgs = {
     headless: "new", //using new headless mode, set to false to disable headless
@@ -43,39 +45,44 @@ const browserArgs = {
 };
 
 (async () => {
-    // const allLoginRecords: ExistingRecord[] = await getAllRecords(loginsTable.id);
 
-    // const inputs: LoginInput[] = allLoginRecords.map((record) => ({
-    //     loginID: record.id,
-    //}));
+    const COMPLETE_UPDATE = true;
 
-    // eslint-disable-next-line max-len
-    const body = JSON.parse("[{\"loginID\":\"66a790491e7c811e8ba54f8a\"}]");
-    // eslint-disable-next-line max-len
-    //,{\"loginID\":\"66aa4b9347dc90578fc2a748\"},{\"loginID\":\"66aa4ba6b46e1cd11fc29370\"},{\"loginID\":\"65e37da7f8428f036fd9978f\"},{\"loginID\":\"65e37da7f8428f036fd99792\"},{\"loginID\":\"65e37da7f8428f036fd99794\"},{\"loginID\":\"65e37da7f8428f036fd997c1\"},{\"loginID\":\"65e37da7f8428f036fd997d3\"},{\"loginID\":\"65e37da7f8428f036fd997d5\"},{\"loginID\":\"65e37da7f8428f036fd997d9\"},{\"loginID\":\"65e37da7f8428f036fd997fc\"},{\"loginID\":\"65e37da7f8428f036fd9980b\"},{\"loginID\":\"65e37da7f8428f036fd9980f\"},{\"loginID\":\"65e37da7f8428f036fd99871\"},{\"loginID\":\"65e37da7f8428f036fd9987f\"},{\"loginID\":\"65e37da7f8428f036fd9990e\"},{\"loginID\":\"65e37da7f8428f036fd99914\"},{\"loginID\":\"65e37da7f8428f036fd9991a\"},{\"loginID\":\"65e37da7f8428f036fd9991b\"},{\"loginID\":\"65e37da7f8428f036fd99927\"},{\"loginID\":\"65e37da7f8428f036fd99956\"},{\"loginID\":\"65e37da7f8428f036fd999e4\"},{\"loginID\":\"65e37da7f8428f036fd999e9\"},{\"loginID\":\"65e37da7f8428f036fd999ea\"},{\"loginID\":\"65e37da7f8428f036fd999ec\"},{\"loginID\":\"65e37da7f8428f036fd999ee\"},{\"loginID\":\"65e37da7f8428f036fd999f0\"},{\"loginID\":\"65e37da7f8428f036fd99a2f\"},{\"loginID\":\"65e37da7f8428f036fd99b69\"},{\"loginID\":\"65e37da7f8428f036fd99b7a\"},{\"loginID\":\"65e37da7f8428f036fd99c02\"},{\"loginID\":\"65e37da7f8428f036fd99c30\"},{\"loginID\":\"65e37da7f8428f036fd99c31\"},{\"loginID\":\"65e37da7f8428f036fd99965\"},{\"loginID\":\"65e37da7f8428f036fd99991\"},{\"loginID\":\"65e37da7f8428f036fd99992\"},{\"loginID\":\"65e37da7f8428f036fd99993\"},{\"loginID\":\"65e37da7f8428f036fd9999a\"},{\"loginID\":\"66951b781728eb4178eb7626\"},{\"loginID\":\"65e37da7f8428f036fd999ad\"},{\"loginID\":\"65e37da7f8428f036fd999af\"},{\"loginID\":\"65e37da7f8428f036fd999b1\"},{\"loginID\":\"65e37da7f8428f036fd999ba\"},{\"loginID\":\"65e37da7f8428f036fd999bd\"},{\"loginID\":\"65e37da7f8428f036fd999c1\"},{\"loginID\":\"65e37da7f8428f036fd999c2\"},{\"loginID\":\"65e37da7f8428f036fd999ca\"},{\"loginID\":\"65e37da7f8428f036fd999d1\"},{\"loginID\":\"65e37da7f8428f036fd999d8\"},{\"loginID\":\"65e37da7f8428f036fd999db\"}]");
-
-    await main(body,
-        puppeteer as unknown as PuppeteerCoreNode,
-        browserArgs,
-        false);
-    return;
-    /* */
-
-    /*
-    async function updateLogins(step: number, index: number = 0) {
-        if (inputs.length - step > index) {
-            console.log(`index ${index}`);
-            await main(inputs.slice(index, index + step),
-                puppeteer as unknown as PuppeteerCoreNode,
-                browserArgs,
-                1,
-                true);
-            updateLogins(step, index + step);
-        } else {
-            await main(inputs.slice(index), puppeteer as unknown as PuppeteerCoreNode, browserArgs, 1, true);
+    if (COMPLETE_UPDATE) {
+        if (process.env.NODE_ENV !== "production") { //set environment variables using local .env
+            require("dotenv").config({ path: '../../.env' });
+            console.log("Using local environment variables")
         }
+
+        const ss = new SmartSuite("s5ch1upc", process.env.TECHNICAL_SMARTSUITE_KEY as string);
+        const allLoginRecords: SmartSuiteRecord[] = await ss.getAllRecords(loginsTable.id);
+
+        const inputs: { loginID: string }[] = allLoginRecords.map((record) => ({
+            loginID: record.id,
+        }));
+
+        async function updateLogins(step: number, index: number = 0) {
+            if (inputs.length - step > index) {
+                console.log(`index ${index}`);
+                await main(inputs.slice(index, index + step),
+                    puppeteer as unknown as PuppeteerCoreNode,
+                    browserArgs);
+                updateLogins(step, index + step);
+            } else {
+                await main(inputs.slice(index), puppeteer as unknown as PuppeteerCoreNode, browserArgs);
+            }
+        }
+
+        updateLogins(100, 0);
+    } else {
+        //change inputs here
+        const body = JSON.parse("[{\"loginID\":\"65e37da7f8428f036fd99bd0\"},{\"loginID\":\"65e37da7f8428f036fd9981b\"},{\"loginID\":\"65e37da7f8428f036fd99856\"},{\"loginID\":\"65e37da7f8428f036fd9985b\"},{\"loginID\":\"65e37da7f8428f036fd99876\"},{\"loginID\":\"65e37da7f8428f036fd99890\"},{\"loginID\":\"65e37da7f8428f036fd998a6\"},{\"loginID\":\"65e37da7f8428f036fd998aa\"},{\"loginID\":\"65e37da7f8428f036fd99921\"},{\"loginID\":\"65e37da7f8428f036fd99979\"},{\"loginID\":\"65e37da7f8428f036fd999c3\"},{\"loginID\":\"65e37da7f8428f036fd99a19\"},{\"loginID\":\"65e37da7f8428f036fd99a1f\"},{\"loginID\":\"65e37da7f8428f036fd99a40\"},{\"loginID\":\"65e37da7f8428f036fd99ac1\"},{\"loginID\":\"65e37da7f8428f036fd99acf\"},{\"loginID\":\"65e37da7f8428f036fd99bea\"},{\"loginID\":\"65e37da7f8428f036fd99bec\"},{\"loginID\":\"66048a2deacc4d19bab05b96\"},{\"loginID\":\"66048a2deacc4d19bab05b97\"},{\"loginID\":\"66048a2deacc4d19bab05b98\"},{\"loginID\":\"66048a2deacc4d19bab05b99\"},{\"loginID\":\"66048a2deacc4d19bab05b9a\"},{\"loginID\":\"66048a2deacc4d19bab05b9b\"},{\"loginID\":\"66048a2deacc4d19bab05b9c\"},{\"loginID\":\"66048a2deacc4d19bab05b9d\"},{\"loginID\":\"66048a2deacc4d19bab05b9e\"},{\"loginID\":\"66048a2deacc4d19bab05b9f\"},{\"loginID\":\"66048a2deacc4d19bab05ba0\"},{\"loginID\":\"66048a2deacc4d19bab05ba2\"},{\"loginID\":\"66048a2deacc4d19bab05ba3\"},{\"loginID\":\"66048a2deacc4d19bab05ba8\"},{\"loginID\":\"66048a2deacc4d19bab05ba9\"},{\"loginID\":\"66048a2deacc4d19bab05bac\"},{\"loginID\":\"66048a2deacc4d19bab05bae\"},{\"loginID\":\"66048a2deacc4d19bab05bb1\"},{\"loginID\":\"66048a2deacc4d19bab05bb2\"},{\"loginID\":\"66048a2deacc4d19bab05bb3\"},{\"loginID\":\"66048a2deacc4d19bab05bb4\"},{\"loginID\":\"66048a2deacc4d19bab05bb5\"},{\"loginID\":\"66048a2deacc4d19bab05bb6\"},{\"loginID\":\"66048a2deacc4d19bab05bb7\"},{\"loginID\":\"66048a2deacc4d19bab05bb8\"},{\"loginID\":\"66048a2deacc4d19bab05bbb\"},{\"loginID\":\"66aa4d60a5815e4e94d5f402\"}]");
+
+        await main(body,
+            puppeteer as unknown as PuppeteerCoreNode,
+            browserArgs,
+            false);
+        return;
     }
 
-    updateLogins(50, 50);
-    */
 })();
