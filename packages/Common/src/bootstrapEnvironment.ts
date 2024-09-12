908b20e567c0edd5d6667d2cf301c3a6aff3557b
cbility <ap.johnston@outlook.com>
2024-09-12 10:10:18 +0100

    change env var initialisation

--- packages/Common/src/bootstrapEnvironment.ts
+++ packages/Common/src/bootstrapEnvironment.ts
@@ -3,12 +3,13 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 export default function bootstrapEnvironment() {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    const relativeDotenvPath = "../../../.env";
-    const resolvedDotenvPath = path.resolve(__dirname, relativeDotenvPath);
-    const result = dotenv.config({ path: resolvedDotenvPath });
-
-    if (result.error) throw result.error;
-    return result;
+    if (process.env.NODE_ENV !== "production") {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const relativeDotenvPath = "../../../.env";
+        const resolvedDotenvPath = path.resolve(__dirname, relativeDotenvPath);
+        const result = dotenv.config({ path: resolvedDotenvPath });
+        if (result.error) throw result.error;
+        return result;
+    }
 }
