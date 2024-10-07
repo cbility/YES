56c060b5f4f66a5ea662c44055681b8cf102dc46
Adam Johnston <ap.johnston@outlook.com>
2024-10-07 10:58:18 +0100

    add test

--- packages/QuickFile/src/tests/getQuickFileInvoice.test.ts
+++ packages/QuickFile/src/tests/getQuickFileInvoice.test.ts
@@ -6,4 +6,4 @@ const QF = new QuickFileAPIHandler("6131405563",
     "8e48b467-333a-46cc-958c-a2f286dad614",
     process.env.QUICKFILE_API_KEY as string);
 
-QF.invoiceGet({ InvoiceID: 33008169 }).then(response => console.log(response.Invoice_Get.Body.InvoiceDetails));
+QF.invoiceGet({ InvoiceID: +process.argv[2] }).then(response => console.log(response.Invoice_Get.Body.InvoiceDetails));
