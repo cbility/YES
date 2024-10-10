e9b2e3a5f58e4da1ba9ee08e75efb5607061d740
Adam Johnston <ap.johnston@outlook.com>
2024-10-10 12:04:50 +0100

    update test

--- packages/Main/src/tests/webhookHandler.test.ts
+++ packages/Main/src/tests/webhookHandler.test.ts
@@ -7,9 +7,9 @@ const testEvent = {
     "rawQueryString": "",
     "headers": {
         "x-amzn-tls-cipher-suite": "ECDHE-RSA-AES128-GCM-SHA256",
-        "content-length": "277",
+        "content-length": "425",
         "x-amzn-tls-version": "TLSv1.2",
-        "x-amzn-trace-id": "Root=1-67063668-2eb7092076c0e0b24054c341",
+        "x-amzn-trace-id": "Root=1-6707aeec-7e8e3116659d22265891aba9",
         "x-forwarded-proto": "https",
         "host": "zzgwpptcirookdzrt7ctu73gs40ouytr.lambda-url.eu-west-2.on.aws",
         "x-forwarded-port": "443",
@@ -29,13 +29,13 @@ const testEvent = {
             "sourceIp": "13.79.122.255",
             "userAgent": "quickfile-webhook-service/1.0"
         },
-        "requestId": "036ad835-4b5e-46bf-a0bb-a645da8e56f3",
+        "requestId": "a1876069-9865-400a-ae23-178f26db3d81",
         "routeKey": "$default",
         "stage": "$default",
-        "time": "09/Oct/2024:07:53:12 +0000",
-        "timeEpoch": 1728460392564
+        "time": "10/Oct/2024:10:39:40 +0000",
+        "timeEpoch": 1728556780996
     },
-    "body": "{\"PayLoad\": {\"InvoicesDeleted\":[{\"TimeStamp\":\"2024-10-09T07:49:13\",\"Id\":33760600},{\"TimeStamp\":\"2024-10-09T07:48:59\",\"Id\":33722445}],\"Timestamp\":\"2024-10-09T07:53:12.3822178+00:00\",\"Signature\":\"70542fd78070ba1dffa2ff0718ae98a2\",\"Hookid\":\"1aaa4fee-0e8d-4172-8a9e-600edd0bfbf2\"}}",
+    "body": "{\"PayLoad\": {\"InvoicesCreated\":[{\"TimeStamp\":\"2024-10-10T10:38:05\",\"Id\":33831297,\"InvoiceType\":\"INV\",\"FromRecurring\":false,\"RecurringParentId\":0}],\"InvoicesViewed\":[{\"TimeStamp\":\"2024-10-10T10:36:29\",\"Id\":33711414,\"InvoiceType\":\"INV\",\"ClientId\":3436826,\"ClientContactId\":1187350}],\"Timestamp\":\"2024-10-10T10:39:40.7222584+00:00\",\"Signature\":\"a318a7216fa39f5f8232113cb44e9164\",\"Hookid\":\"4181d83b-4966-4ebc-b283-c7dd089a246f\"}}",
     "isBase64Encoded": false
 }
 
